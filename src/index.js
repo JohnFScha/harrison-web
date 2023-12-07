1aed1b65ee7335e6734a82c2dba3271c57f90897
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-12-07 17:13:55 -0300

    testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* Imports */
 
-import Lenis from '../node_modules/@studio-freight/lenis/dist/lenis.mjs'
+import Lenis from "../node_modules/@studio-freight/lenis/dist/lenis.mjs";
 import gsap from "../node_modules/gsap/index.js";
 import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
 
@@ -10,28 +10,64 @@ gsap.registerPlugin(ScrollTrigger);
 
 /* Lenis config */
 
-const lenis = new Lenis()
+const lenis = new Lenis();
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
 
 /* Gsap instantiation */
 
-let tl = gsap.timeline()
+let tl = gsap.timeline();
 
-tl.from(".text", {
+tl.from(".text-1", {
   y: 50,
   opacity: 0,
   scrollTrigger: {
-    trigger: '.text',
-    start: '10% center',
-    end: 'bottom center',
+    trigger: ".text-1",
+    start: "-200% center",
+    end: "-100% center",
+    scrub: true,
+    pinSpacing: false,
+  },
+});
+
+tl.from(".text-2", {
+  y: 50,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".text-2",
+    start: "-200% center",
+    end: "-100% center",
+    scrub: true,
+    pinSpacing: false,
+  },
+});
+
+tl.from(".text-3", {
+  y: 50,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".text-3",
+    start: "-200% center",
+    end: "-150% center",
     scrub: true,
     markers: true,
-    pinSpacing: false
-  }
-})
+    pinSpacing: false,
+  },
+});
+
+tl.to("#section-1 img", {
+  scale: 1.5, // Adjust the scale factor for zooming
+  transformOrigin: "bottom left", // Zoom to the bottom left corner
+  scrollTrigger: {
+    trigger: "#section-1",
+    start: "50% top",
+    end: "bottom bottom",
+    scrub: true,
+    markers: true
+  },
+});
