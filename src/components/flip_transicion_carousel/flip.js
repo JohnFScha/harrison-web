49ab575c63bb00edd51fc7597e4af0b1bbc5eddd
Fermin Vazquez <fervazquez.dev@gmail.com>
2023-12-15 19:55:53 -0300

    vinculos+carousel

--- src/components/flip_transicion_carousel/flip.js
+++ src/components/flip_transicion_carousel/flip.js
@@ -2,6 +2,13 @@ import gsap from "../../../node_modules/gsap/index.js";
 import { ScrollTrigger } from "../../../node_modules/gsap/ScrollTrigger.js";
 import Lenis from "../../../node_modules/@studio-freight/lenis/dist/lenis.mjs";
 
+const carouselCtn = document.getElementById("carousel-container");
+const carousel = document.getElementById("carouselFig");
+
+if (carouselCtn.style.transform === "scale(1)") {
+  carousel.style.animation = "rotateAnim 30s infinite forwards";
+}
+
 gsap.registerPlugin(ScrollTrigger);
 
 const lenis = new Lenis();
@@ -17,13 +24,14 @@ let textTl = gsap.timeline({
   scrollTrigger: {
     trigger: "#text-container",
     start: "top top",
-    end: "bottom+=360%",
+    end: "bottom+=200%",
     scrub: true,
-    markers: true,
     pin: true,
   },
 });
-textTl.to("#p1", { x: 0 }).to("#p2", { x: 0 });
-textTl.to("#p1", { x: -2000 });
-textTl.to("#p2", { y: 56 });
-textTl.to("#carousel", { fontSize: "3rem" });
+textTl.staggerTo(["#p1", "#p2"], 1.1, { x: 0 }, 0.2);
+textTl.staggerTo(["#p1"], 1.5, { x: -2000 }, 0);
+textTl.to("#p2", { y: 48 }, "-=1");
+textTl
+  .to("#carousel-container", { transform: "scale(0.75)" }, "-=1")
+  .timeScale(1.5);
