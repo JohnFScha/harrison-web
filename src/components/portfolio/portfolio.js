25cde782295cbca79ad68f07ebddf417e47555e0
Fermin Vazquez <fervazquez.dev@gmail.com>
2023-12-17 16:38:15 -0300

    modales listossss

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,52 +1,74 @@
 /* Imports */
 import gsap from "../../../node_modules/gsap/index.js";
 
-let titleCtn = document.querySelectorAll("section.portfolio h2");
+let ctn = document.querySelectorAll("section.portfolio li");
 let descCtn = document.querySelectorAll(".desc-ctn");
 let hr = document.querySelectorAll("hr.ctn-line");
-const customCursor = document.createElement('div');
-customCursor.className = 'hand-cursor';
 
-// Append customCursor to the body
-document.body.appendChild(customCursor);
-
-titleCtn.forEach((element, index) => {
-
-    element.addEventListener('mouseover', (event) => hover(event, index));
-    element.addEventListener('mouseout', (event) => out(event, index));
-    element.addEventListener('mousemove', (event) => {
-      // Update the position of the custom cursor based on the mouse pointer
-      customCursor.style.left = event.pageX + 'px';
-      customCursor.style.top = event.pageY + 'px';
-    });
+ctn.forEach((element, index) => {
+  element.addEventListener("mouseover", (event) => hover(event, index));
+  element.addEventListener("mouseout", (event) => out(event, index));
 });
 
 function hover(event, index) {
-  gsap.to(hr[index], 0.5, {
-    margin: '20 0 10 0'
+  gsap.to(hr[index], 0.7, {
+    margin: "50 0 10 0",
   });
-  gsap.to(descCtn[index], 0.3, {
+  gsap.to(descCtn[index], 0.5, {
     y: 0,
     opacity: 1,
-    margin: '10 0 50 0'
   });
-  gsap.to(titleCtn[index], 0.3, {
-    color: 'rgb(203, 219, 67)'
-  });
-  customCursor.style.display = 'block';
 }
 
 function out(event, index) {
-  gsap.to(hr[index], 0.5, {
-    margin: '-12 0'
+  gsap.to(hr[index], 0.7, {
+    margin: "10 0",
   });
-  gsap.to(descCtn[index], 0.3, {
+  gsap.to(descCtn[index], 0.5, {
     y: -100,
     opacity: 0,
-    margin: 0
   });
-  gsap.to(titleCtn[index], 0.3, {
-    color: 'transparent'
-  });
-  customCursor.style.display = 'none';
 }
+
+//modales
+const liElements = [];
+liElements.push(document.getElementById("eugenie"));
+liElements.push(document.getElementById("delsud"));
+liElements.push(document.getElementById("flexy"));
+
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modalContent");
+const videoEl = document.getElementById("modalVideo");
+const closeModal = document.getElementById("closeModal");
+
+const videos = [
+  "../../assets/Videos/casoEugenie.mp4",
+  "../../assets/Videos/casoDesarrollos.mp4",
+  "../../assets/Videos/casoFlexy.mp4",
+];
+let currentVideo = null;
+liElements.forEach((liElement, index) =>
+  liElement.addEventListener("click", () => {
+    if (currentVideo && currentVideo.parentNode) {
+      currentVideo.parentNode.removeChild(currentVideo);
+    }
+    let newVideo = document.createElement("video");
+    newVideo.id = "modalVideo";
+    let swapSrc = document.createElement("source");
+    swapSrc.src = videos[index];
+    newVideo.appendChild(swapSrc);
+    modalContent.appendChild(newVideo);
+    modal.classList.add("shown");
+    newVideo.play();
+    currentVideo = newVideo;
+  })
+);
+
+closeModal.addEventListener("click", () => {
+  if (currentVideo) {
+    currentVideo.pause();
+    currentVideo.parentNode.removeChild(currentVideo);
+    currentVideo = null;
+  }
+  modal.classList.remove("shown");
+});
