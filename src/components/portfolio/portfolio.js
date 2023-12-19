a79bee4715a20d7736e557f02aa0bb804904d994
Marcos Iacovone <marcos_iacovone2006@hotmail.com>
2023-12-19 15:21:23 -0300

    portfolio scroll update

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,7 +1,10 @@
 /* Imports */
+import Lenis from "../../../node_modules/@studio-freight/lenis/dist/lenis.mjs";
 import gsap from "../../../node_modules/gsap/index.js";
+import { ScrollTrigger } from "../../../node_modules/gsap/ScrollTrigger.js";
 
-let ctn = document.querySelectorAll("section.portfolio li");
+let txtCtn = document.querySelectorAll(".text-ctn-1");
+let titleCtn = document.querySelectorAll("section.portfolio li h2");
 let descCtn = document.querySelectorAll(".desc-ctn");
 let ctnHr = document.querySelectorAll("hr.ctn-line");
 let previewVideos = document.querySelectorAll(".preview-video");
@@ -10,57 +13,152 @@ let videoOverlay = document.querySelector(".vid-overlay");
 const customCursor = document.createElement("div");
 customCursor.className = "hand-cursor";
 
-ctn.forEach((element, index) => {
-  element.addEventListener("mouseover", (event) => hover(event, index));
-  element.addEventListener("mouseout", (event) => out(event, index));
+/* gsap config */
+
+gsap.registerPlugin(ScrollTrigger);
+
+/* Lenis config */
+
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+/* Gsap instantiation */
+
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: txtCtn,
+//     start: "top+=10% center",
+//     end: "bottom+=200% bottom",
+//     scrub: true,
+//     markers: true,
+//     pin: true,
+//     onUpdate: ({ progress, direction, isActive }) => {
+//       // Perform actions based on the scrubbed timeline progress
+//     },
+//   },
+// });
+
+gsap.to(".sup-rodaje", {
+  scrollTrigger: {
+  trigger: ".sup-rodaje",
+  start: "top top",
+  end: "bottom bottom",
+  scrub: true,
+  pin: txtCtn,
+  },
+});
+
+gsap.to(".txt-ctn-1", {
+  scrollTrigger: {
+    trigger: ".txt-ctn-1",
+    start: "top center",
+    end: "bottom+=100% bottom",
+    scrub: true,
+    pin: txtCtn,
+    markers: true,
+  },
+});
+
+gsap.to(".sup-rodaje", {
+  top: 0
+});
+
+gsap.to(".txt-row:nth-child(1)", {
+  opacity: 1,
+  // duration: 5,
+});
+
+gsap.to(".txt-row:nth-child(2)", {
+  opacity: 1,
+});
+
+
+// // Fade in animation for the newly added p elements
+// tl.staggerFrom(".bg-rodaje", 1, {
+//   opacity: 0,
+//   // x: -50, // Move from left to right
+// }, 0.5); // Stagger the fade-in with a delay of 0.5 seconds between each paragraph
+
+// // Define the zoom-in animation for the image
+// tl.to(".sup-rodaje", {
+//   // scale: 1.5, // Adjust the scale factor for zooming
+//   // transformOrigin: "bottom left", // Zoom to the bottom left corner
+// });
+
+// Append customCursor to the body
+document.body.appendChild(customCursor);
+
+titleCtn.forEach((element, index) => {
+
+  element.addEventListener('mouseover', (event) => hover(event, index));
+  element.addEventListener('mouseout', (event) => out(event, index));
+  element.addEventListener('mousemove', (event) => {
+    // Update the position of the custom cursor based on the mouse pointer
+    customCursor.style.left = event.pageX + 'px';
+    customCursor.style.top = event.pageY + 'px';
+  });
 });
 
 function hover(event, index) {
   gsap.to(ctnHr[index], 0.5, {
-    margin: "20 0 10 0",
+    margin: '20 0 10 0'
   });
-  gsap.to(descCtn[index], 0.5, {
+  gsap.to(descCtn[index], 0.3, {
     y: 0,
     opacity: 1,
+    // margin: '10 0 50 0'
   });
   gsap.to(titleCtn[index], 0.3, {
-    color: "rgb(203, 219, 67)",
+    color: 'rgb(203, 219, 67)',
+    height: 100
+  });
+  gsap.to(titleCtn[1], 0.7, {
+    height: 190
   });
   gsap.to(previewVideos[index], 0.4, {
-    opacity: 1,
+    opacity: 1
   });
   gsap.to(videoOverlay, 0.4, {
-    opacity: 0.4,
+    opacity: 0.4
   });
   gsap.to(logoPortfolio, 0.4, {
-    opacity: 0,
+    opacity: 0
   });
-  customCursor.style.display = "block";
+  customCursor.style.display = 'block';
 }
 
 function out(event, index) {
   gsap.to(ctnHr[index], 0.5, {
-    margin: "-12 0",
+    margin: '-12 0'
   });
-  gsap.to(descCtn[index], 0.5, {
+  gsap.to(descCtn[index], 0.3, {
     y: -100,
     opacity: 0,
+    // margin: 0
   });
   gsap.to(titleCtn[index], 0.3, {
-    color: "transparent",
+    color: 'transparent',
+    height: 'auto'
   });
   gsap.to(previewVideos[index], 0.4, {
-    opacity: 0,
+    opacity: 0
   });
   gsap.to(videoOverlay, 0.4, {
-    opacity: 1,
+    opacity: 1
   });
   gsap.to(logoPortfolio, 0.4, {
-    opacity: 0.4,
+    opacity: 0.4
   });
-  customCursor.style.display = "none";
+  customCursor.style.display = 'none';
 }
-//modales
+
+// modals
 const liElements = [];
 liElements.push(document.getElementById("eugenie"));
 liElements.push(document.getElementById("delsud"));
@@ -72,9 +170,9 @@ const videoEl = document.getElementById("modalVideo");
 const closeModal = document.getElementById("closeModal");
 
 const videos = [
-  "../../assets/Videos/casoEugenie.mp4",
-  "../../assets/Videos/casoDesarrollos.mp4",
-  "../../assets/Videos/casoFlexy.mp4",
+  "../../assets/casos/eugenie-comp.mp4",
+  "../../assets/casos/delsud-comp.mp4",
+  "../../assets/casos/flexy-comp.mp4",
 ];
 let currentVideo = null;
 liElements.forEach((liElement, index) =>
