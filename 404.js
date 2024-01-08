fd43921f2e7924fb88cfea907e89af7716247c04
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-01-08 14:24:14 -0300

    404 mobile

--- 404.js
+++ 404.js
@@ -1,5 +1,9 @@
 const back = document.getElementById('back-btn')
 
+if (window.innerWidth >= 390) {
+  back.innerText = 'Volver a intentar'
+}
+
 back.addEventListener('click', () => {
   window.history.back()
 })
